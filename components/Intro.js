62a5eae6eb74b87d143bb2f624e40dc065d4d9c7
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-07-24 19:42:52 +0430

    changed some textes

--- components/Intro.js
+++ components/Intro.js
@@ -4,7 +4,7 @@ import AnimatedDiv from './AnimatedDiv'
 const Intro = () => {
     return (
         <AnimatePresence>
-            <div className="flex justify-start items-center px-8 md:px-32 my-24">
+            <div className="flex justify-start items-center px-8 md:px-32 my-48">
                 <div className="text-right">
                     <AnimatedDiv delay={0.3}>
                         <p className="text-primary text-lg font-body">
@@ -17,26 +17,25 @@ const Intro = () => {
                             </span>
                         </p>
                         <p className="text-dark-gray text-2xl md:text-4xl font-body mt-10">
-                            من طراح و توسعه دهنده نرم افزار های کاربردی هستم.
+                            من طراح و توسعه دهنده فرانت-اند هستم.{' '}
                         </p>
                     </AnimatedDiv>
                     <AnimatedDiv delay={0.5}>
                         <p className="text-dark-gray text-xl font-body mt-10 pl-14 lg:w-9/12 text-justify opacity-80">
-                            شاید باور کردنش سخت باشه ولی از ابتدای دوران مدرن
-                            سخت افزار های کامپیوتر های ما بسیار کم نسبت به نرم
-                            افزار های ما پیشرفت کردن. در واقع سخت افزار های ما
-                            در حد چند تا عمل ساده ریاضی باقی می موندن اگر نرم
-                            افزاری نبود که روشون سوار بشه و با همون چنتا عمل
-                            ساده ریاضی محاسبات پیچیده انجام بده و من عمیقا شیفته
-                            این کار هستم که ماشین هارو تا نهایت حد توانشون به
-                            کار بگیرم.
+                            از نوشتن ابزار های کوچیک که باعث میشه انجام دادن یه
+                            کار آسون بشه تا نرم افزار های بسیار پیچیده بخش بزرگی
+                            از علایق من در زندگی هست. در حال حاضر به صورت فری
+                            لنسر کار می کنم ولی آماده دریافت پیشنهادات کاری هم
+                            هستم.
                         </p>
                         <div className="mt-14 font-body font-bold text-xl">
-                            <Button
-                                color="primary"
-                                text="تماس با من"
-                                borderRadius="5px"
-                            />
+                            <a href="#contact">
+                                <Button
+                                    color="primary"
+                                    text="تماس با من"
+                                    borderRadius="5px"
+                                />
+                            </a>
                         </div>
                     </AnimatedDiv>
                 </div>
