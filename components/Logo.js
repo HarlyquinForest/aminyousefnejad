991fe21925c0dbff877383be7e1d7231e14447aa
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-07-24 19:39:47 +0430

    fix link of logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,13 +1,13 @@
 import NextLink from 'next/link'
 const Logo = () => {
     return (
-        <div className="p-1 px-3 rounded-lg border-2 border-primary">
-            <NextLink href="/">
-                <a>
+        <NextLink href="/">
+            <a>
+                <div className="p-1 px-3 rounded-lg border-2 border-primary">
                     <h1 className="text-primary font-bold">A</h1>
-                </a>
-            </NextLink>
-        </div>
+                </div>{' '}
+            </a>
+        </NextLink>
     )
 }
 export default Logo
