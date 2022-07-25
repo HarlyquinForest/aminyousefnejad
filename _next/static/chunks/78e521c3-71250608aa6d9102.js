472a5cd34af7f8f4c51e199fc4ec5e019e474de5
HarlyquinForest <36879917+HarlyquinForest@users.noreply.github.com>
2022-07-25 09:02:43 +0000

    Deploying to gh-pages from @ HarlyquinForest/aminyousefnejad@04b8c0700e7bf3afba5b6d0595148ad7a8d07a17 🚀

---
+++ _next/static/chunks/78e521c3-71250608aa6d9102.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[937],{9352:function(t,a,n){n.d(a,{jZo:function(){return e}});var r=n(8357);function e(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M16 12l-6 6V6z"}}]}]})(t)}}}]);
