35943a854160295e914ffa286798bbed8ea4bcdf
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-07-28 22:41:01 +0430

    border-primary fix white unexpected border

--- components/Button.js
+++ components/Button.js
@@ -15,7 +15,7 @@ const Button = ({
             type={type ? type : 'button'}
             onClick={clickHandler}
             style={{ borderRadius }}
-            className={`flex flex-row items-center text-${size} p-2 w-${width} font-extralight border-1 text-${color} border-${color}`}
+            className={`flex flex-row items-center text-${size} p-2 w-${width} font-extralight border-1 text-${color} border-primary`}
         >
             {icon && <span className="ml-2 text-2xl">{icon}</span>}
             {text}
