d7f5f7ac07e8edeebff46bbfefc1f0e0bafa01e5
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-07-29 18:23:25 +0430

    width of footer fixed

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="flex flex-col items-center w-full font-body text-dark-gray py-8 mt-12 ">
+        <div className="flex flex-col items-center w-full font-body text-dark-gray py-8 px-16  mt-12 ">
             <p>
                 طراحی و توسعه این صفحه توسط امین یوسف نژاد انجام گرفته و هرگونه
                 بهره برداری مطابق لایسنس GPL بلامانع است. :)
