36946a817c284cccf46ee910c9810fad7e154dcb
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-07-29 18:26:59 +0430

    some styles update to make Section component more reusable

--- components/Section.js
+++ components/Section.js
@@ -5,26 +5,23 @@ import { motion } from 'framer-motion'
 const Section = ({ children, title, image, className, id }) => {
     return (
         <AnimatePresence>
-            <div
-                className={`flex flex-col w-11/12 lg:w-9/12 mx-2 p-8 ${className}`}
-                id={id}
-            >
+            <div className={`flex flex-col mx-2 p-4 ${className}`} id={id}>
                 <div className="flex flex-row justify-start items-center w-full">
                     <span className="text-primary ml-2">
                         <GoPrimitiveDot />
                     </span>
-                    <h1 className=" text-dark-gray  text-xl lg:text-4xl font-body w-max min-w-fit">
+                    <h1 className=" text-light-gray  text-xl lg:text-2xl font-body w-max min-w-fit">
                         {title}
                     </h1>
                     <div className="h-0 border-t-1 w-full border-dark-gray mr-4"></div>
                 </div>
                 <div className="flex flex-col w-full lg:flex-row items-center px-6 lg:items-start justify-between text-light-gray text-right mt-4">
                     {image && (
-                        <div className="img-hover-zoom--slowmo max-w-300 xl:max-w-md order-1 lg:order-2 rounded-lg">
+                        <div className="img-hover-zoom--slowmo max-w-200 xl:max-w-xs order-1 lg:order-2 rounded-lg mt-6">
                             <img
                                 src={image}
                                 alt="avatar picture"
-                                className="drop-shadow-xl transition-all delay-200 cursor-none"
+                                className="drop-shadow-xl transition-all delay-200 cursor-none "
                             />
                         </div>
                     )}
@@ -37,6 +34,6 @@ const Section = ({ children, title, image, className, id }) => {
     )
 }
 export const Paragraph = ({ children }) => {
-    return <p className="text-justify">{children}</p>
+    return <p className="text-justify mt-2">{children}</p>
 }
 export default Section
