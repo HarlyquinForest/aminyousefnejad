c719dac120fc3010361d20491800cbaea45a2ca7
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-07-30 19:31:07 +0430

    add Buy me a coffee button

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,17 @@
+import { AiOutlineCoffee } from 'react-icons/ai'
 const Footer = () => {
     return (
-        <div className="flex flex-col items-center w-full font-body text-dark-gray py-8 px-16  mt-12 ">
+        <div className="flex flex-col items-center w-full font-body gap-8 text-dark-gray py-8 px-16  mt-12 ">
+            <a href="http://www.coffeete.ir/amin-yousefnejad" target="_blank">
+                <div className="flex flex-row items-start gap-3 p-3 text-amber-900 font-bold rounded-lg bg-secondary hover:bg-amber-600 hover:translate-y-2 transition-all">
+                    <span className="inline-block items-center font-dancing text-lg">
+                        Buy Me a Coffe
+                    </span>
+                    <span className="text-2xl inline-block">
+                        <AiOutlineCoffee />
+                    </span>
+                </div>
+            </a>
             <p>
                 طراحی و توسعه این صفحه توسط امین یوسف نژاد انجام گرفته و هرگونه
                 بهره برداری مطابق لایسنس GPL بلامانع است. :)
