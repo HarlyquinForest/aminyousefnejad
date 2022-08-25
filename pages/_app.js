72f28bd1f5788e28a9a755f3577303b406bef394
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-08-25 12:53:51 +0430

    _app.js cleaned and hase nothing to do with animation and shit

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,11 @@
 import { ContextProvider } from '../context/ContextProvider'
 import '../public/styles/global.css'
 import Layout from '../components/layout/Main'
-import { useEffect, useState } from 'react'
-import Landing from '../components/Landing'
-import { useAnimation } from 'framer-motion'
 
 function MyApp({ Component, pageProps, router }) {
-    const [loading, setLoading] = useState(true)
-    const [ready, setReady] = useState(false)
-    const landingControl = useAnimation()
-    const layoutControl = useAnimation()
-    useEffect(() => {
-        setTimeout(() => setLoading(false), 5000)
-    }, [])
-    useEffect(() => {
-        if (!loading) landingControl.start('hidden')
-        if (!loading) setTimeout(() => layoutControl.start('visible'), 800)
-        if (!loading) setTimeout(() => setReady(true), 1500)
-    }, [loading])
     return (
         <ContextProvider>
-            {!ready && <Landing animate={landingControl} />}
-            <Layout router={router} animate={layoutControl} ready={ready}>
+            <Layout router={router}>
                 <Component {...pageProps} />
             </Layout>
         </ContextProvider>
