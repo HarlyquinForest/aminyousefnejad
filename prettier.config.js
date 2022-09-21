21b7073fc88855069c18eacd5d281ec1b6317d99
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-09-21 22:19:06 +0430

    install and configure prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 const options = {
-    arrowParens: 'avoid',
-    singleQuote: true,
-    bracketSpacing: true,
-    endOfLine: 'lf',
-    semi: false,
-    tabWidth: 4,
-    trailingComma: 'none'
+  arrowParens: 'avoid',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 4,
+  trailingComma: 'none'
 }
 module.exports = options
