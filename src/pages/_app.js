d386465a8686006cf5d97874d60c1def9b3af22c
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-09-21 22:19:51 +0430

    remove unnecessary codes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,6 @@ import '../../public/styles/global.css'
 import Layout from '../common/layout/Main'
 
 function MyApp({ Component, pageProps, router }) {
-  console.log("app.js")
     return (
         <ContextProvider>
             <Layout router={router}>
