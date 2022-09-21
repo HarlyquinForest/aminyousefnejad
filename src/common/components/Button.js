8cdeae9b329b15065124bbeb5846802ca72bd5f7
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-09-21 23:33:59 +0430

    font style changed to bold

--- src/common/components/Button.js
+++ src/common/components/Button.js
@@ -1,26 +1,26 @@
 const Button = ({
-    icon,
-    bgColor,
-    type,
-    color,
-    size,
-    bgHoverColor,
-    text,
-    borderRadius,
-    width,
-    clickHandler
+  icon,
+  bgColor,
+  type,
+  color,
+  size,
+  bgHoverColor,
+  text,
+  borderRadius,
+  width,
+  clickHandler
 }) => {
-    return (
-        <button
-            type={type ? type : 'button'}
-            onClick={clickHandler}
-            style={{ borderRadius }}
-            className={`flex flex-row items-center text-${size} p-2 w-${width} font-light border-1 text-${color} border-primary hover:bg-primary hover:text-light-gray hover:translate-y-1 transition-all`}
-        >
-            {icon && <span className="text-2xl">{icon}</span>}
-            {text && text}
-        </button>
-    )
+  return (
+    <button
+      type={type ? type : 'button'}
+      onClick={clickHandler}
+      style={{ borderRadius }}
+      className={`flex flex-row items-center text-${size} p-2 w-${width} font-semibold border-2 text-${color} border-primary hover:bg-primary hover:text-light-gray hover:translate-y-1 transition-all`}
+    >
+      {icon && <span className="text-2xl">{icon}</span>}
+      {text && text}
+    </button>
+  )
 }
 
 export default Button
